a53e5bba4c891f825de930c0e328ddd14b8c1092
pengs <121928419+pengsies@users.noreply.github.com>
2023-07-23 19:37:39 +0800

    Update core.py

--- SolveIt_backend/backend/app/models/core.py
+++ SolveIt_backend/backend/app/models/core.py
@@ -1,36 +1,48 @@
-# backend/app/models/core.py
-
 from app.db.base_class import Base
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, Mapped, mapped_column
-from app.crud.base import CRUD
+from sqlalchemy.orm import relationship
 
-class User(Base, CRUD["User"]):
+class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(
-        primary_key=True,
-        index=True,
-    )
-    username: Mapped[str] = mapped_column(unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(default=True)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
 
-    challenges = relationship("Challenge", back_populates="user")
+    attempts = relationship("Attempt", back_populates="user")
 
-
-class Challenge(Base, CRUD["Challenge"]):
+class Challenge(Base):
     __tablename__ = "challenges"
 
-    id: Mapped[int] = mapped_column(
-        primary_key=True,
-        index=True,
-    )
-    title: Mapped[str] = mapped_column(nullable=False)
-    description: Mapped[str] = mapped_column(nullable=False)
-    category: Mapped[str] = mapped_column(nullable=False)
-    points: Mapped[int] = mapped_column(nullable=False)
-    flag_hash: Mapped[str] = mapped_column(nullable=True)
+    chall_id = Column(Integer, primary_key=True, index=True)
+    chall_title = Column(String, nullable=False)
+    chall_descrip = Column(String, nullable=False)
+    chall_category = Column(String, nullable=False)
+    chall_points = Column(Integer, nullable=False)
+    flag_hash = Column(String, nullable=False)
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user = relationship("User", back_populates="challenges")
+    user_id = Column(Integer, ForeignKey("users.id"))
+    author = relationship("User", back_populates="created_challenges")
+    attempts = relationship("Attempt", back_populates="challenge")
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    
+    challenges = relationship("Challenge", back_populates="category")
+
+class Attempt(Base):
+    __tablename__ = "attempts"
+
+    attempt_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    chall_id = Column(Integer, ForeignKey("challenges.id"))
+    submitted_flag = Column(String, nullable=False)
+    attempt_success = Column(Boolean, nullable=False)
+    
+    user = relationship("User", back_populates="attempts")
+    challenge = relationship("Challenge", back_populates="attempts")
+
