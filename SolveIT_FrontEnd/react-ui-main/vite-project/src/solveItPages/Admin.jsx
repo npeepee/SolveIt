5751ddc6e77f2aa62a0695270f3dca8bd29debb1
pengs <pengsies@gmail.com>
2023-08-16 17:26:33 +0800

    e
    Co-authored-by: ZKBKFFL <ZKBKFFL@users.noreply.github.com>

--- SolveIT_FrontEnd/react-ui-main/vite-project/src/solveItPages/Admin.jsx
+++ SolveIT_FrontEnd/react-ui-main/vite-project/src/solveItPages/Admin.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useContext} from "react";
+import {adminUpdateRole} from "../api/auth/AdminUpdateRole.js";
+import { AuthContext } from "../solveItComponents/AuthProvider";
 
 export default function Admin() {
-  return (
+  const { user, login } = useContext(AuthContext);
+
+  let component
+
+  if (user) {
+    component =
     <>
       <link rel="stylesheet" href="../css/styles.css" />
       <link rel="stylesheet" href="../css/Bootstrap.css" />
       <section className="content p-5 text-center">
-        <p>SolveIt is not configured admin page yeti.</p>
+        <p>Update User Role</p>
+
       </section>
     </>
+  } else {
+    component = <p>Not Authorized</p>
+  }
+  return (
+    {component}
   );
+
+      //let currentTime = new Date()
+
 }
